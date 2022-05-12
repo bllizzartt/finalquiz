ef358285b7ee8a948859cff04c29cf1cf3eb6a99
chase.haynes <chase.haynez@gmail.com>
2022-05-12 12:12:05 -0400

    working hard

--- index.js
+++ index.js
@@ -13,13 +13,32 @@ document.addEventListener("DOMContentLoaded",function(){
 
     console.log(questions[counter].firstElementChild.innerHTML);
 
+let correctScore = 0;
 
 function nextQuestion(){
     // choose the question from the question list, according to the counter
-    localStorage.setItem(questions[counter].firstElementChild.innerHTML, 'value from radio' );
+    let answer = document.getElementsByClassName(`answer${counter}`);
+    console.log(answer);
+
+let userValue;
+
+    for(let i = 0; i < answer.length; i++){
+        if(answer[i].checked){
+            userValue = answer[i].value;
+
+            if(answer[i].classList.contains('correct')){
+                correctScore++;        
+            }
+        }
+    }
+
+    localStorage.setItem(questions[counter].firstElementChild.innerHTML, userValue);
     questions[counter].classList.remove('show');
     counter++;
     questions[counter].classList.add('show');
+
+    showScore();
+
 };
 
 function prevQuestion(){
@@ -28,3 +47,11 @@ function prevQuestion(){
     questions[counter].classList.add('show');
 
 }
+
+
+function showScore(){
+    console.log(correctScore * 10 + "%");
+}
+
+document.getElementById('results').innerHTML = correctScore * 10 + '%';
+console.log(document.getElementById('results'));
