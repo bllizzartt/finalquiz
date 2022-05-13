cf1e6d477cd349b2a6f02bbb2f1365b7f676a337
Chase_Haynes <chase.haynez@gmail.com>
2022-05-13 12:10:18 -0400

    proper results

--- end.js
+++ end.js
@@ -2,12 +2,12 @@
 let resultText = document.querySelector('#final');
     resultText.innerText = localStorage.getItem('correctScore') * 10 + "%";
 
-if(localStorage.getItem('correctScore') > 7){
+if(localStorage.getItem('correctScore') >= 7){
     let outcome = document.getElementsByClassName('outcome')[0];
     outcome.innerText = 'You must know Chase pretty well! Pass!';
     document.body.style.background = 'green';
 
-}else if(localStorage.getItem('correctScore') < 6){
+}else if(localStorage.getItem('correctScore') <= 6){
     let outcome = document.getElementsByClassName('outcome')[0];
     outcome.innerText = 'Seems like you two just met! Fail!';
     document.body.style.background = 'red';
